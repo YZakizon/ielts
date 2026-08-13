abfd80a31a5bd8b30a4ae6c1258aab799a1dc0af
YZakizon <betung07@gmail.com>
2026-08-13 13:37:09 -0700

    Add Australian vocabulary English variant

--- app.js
+++ app.js
@@ -774,8 +774,9 @@ let appStarted = false;
 let authMode = "signup";
 
 const englishVariantLabels = {
-  us: "US English",
-  british: "British English",
+  us: "English (US)",
+  british: "English (UK)",
+  australian: "English (AU)",
 };
 
 const translationLanguageLabels = {
--- server.js
+++ server.js
@@ -76,8 +76,9 @@ const labels = {
 };
 
 const englishVariantLabels = {
-  us: "US English",
-  british: "British English",
+  us: "English (US)",
+  british: "English (UK)",
+  australian: "English (AU)",
 };
 
 const translationLanguageLabels = {
